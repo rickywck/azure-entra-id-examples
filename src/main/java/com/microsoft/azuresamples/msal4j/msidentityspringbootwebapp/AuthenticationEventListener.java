b60188d190708e3f62e0d1eedb7a2a34d5c13556
Ricky Wong <ricky.c.wong@accenture.com>
2025-05-26 15:21:36 -0400

    Fix errors introduced by Jules

--- src/main/java/com/microsoft/azuresamples/msal4j/msidentityspringbootwebapp/AuthenticationEventListener.java
+++ src/main/java/com/microsoft/azuresamples/msal4j/msidentityspringbootwebapp/AuthenticationEventListener.java
@@ -3,49 +3,55 @@ package com.microsoft.azuresamples.msal4j.msidentityspringbootwebapp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.event.AbstractAuthenticationFailureEvent;
-import org.springframework.security.authentication.event.AuthenticationEvent;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.context.ApplicationEvent;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.lang.NonNull;
 
-import javax.servlet.http.HttpServletRequest;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
 @Component
-public class AuthenticationEventListener implements ApplicationListener<AuthenticationEvent> {
-
-    @Autowired
-    private LoginAttemptLogger loginAttemptLogger;
-
-    @Autowired
-    private HttpServletRequest request;
+public class AuthenticationEventListener implements ApplicationListener<ApplicationEvent> {
+    private static final String UNKNOWN = "unknown";
+    private final LoginAttemptLogger loginAttemptLogger;
 
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
 
+    public AuthenticationEventListener(LoginAttemptLogger loginAttemptLogger) {
+        this.loginAttemptLogger = loginAttemptLogger;
+    }
+
     @Override
-    public void onApplicationEvent(AuthenticationEvent event) {
-        String loginId = "unknown";
-        if (event.getAuthentication() != null && event.getAuthentication().getName() != null) {
-            loginId = event.getAuthentication().getName();
-        }
-
-        String ipAddress = "unknown";
-        if (request != null && request.getRemoteAddr() != null) {
-            ipAddress = request.getRemoteAddr();
-        }
-
-        String currentDateTimeUTC = OffsetDateTime.now(ZoneOffset.UTC).format(DATE_TIME_FORMATTER);
+    public void onApplicationEvent(@NonNull ApplicationEvent event) {
+        String loginId = UNKNOWN;
         String status;
-
-        if (event instanceof AuthenticationSuccessEvent) {
+        if (event instanceof AuthenticationSuccessEvent successEvent) {
+            if (successEvent.getAuthentication() != null && successEvent.getAuthentication().getName() != null) {
+                loginId = successEvent.getAuthentication().getName();
+            }
             status = "SUCCESS";
-        } else if (event instanceof AbstractAuthenticationFailureEvent) {
+        } else if (event instanceof AbstractAuthenticationFailureEvent failureEvent) {
+            if (failureEvent.getAuthentication() != null && failureEvent.getAuthentication().getName() != null) {
+                loginId = failureEvent.getAuthentication().getName();
+            }
             status = "FAILURE";
         } else {
             return; // Do not log other types of authentication events
         }
-
+        String ipAddress = getClientIpAddress();
+        String currentDateTimeUTC = OffsetDateTime.now(ZoneOffset.UTC).format(DATE_TIME_FORMATTER);
         loginAttemptLogger.log(loginId, currentDateTimeUTC, ipAddress, status);
+    }
+
+    private String getClientIpAddress() {
+        ServletRequestAttributes attrs = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attrs != null) {
+            return attrs.getRequest().getRemoteAddr();
+        }
+        return UNKNOWN;
     }
 }
